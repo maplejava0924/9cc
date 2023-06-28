3a28a0ff77f8dd7540f23ff8dc79495041913b55
maplejava0924 <maplements@gmail.com>
2023-06-28 15:58:34 +0000

    created compiler to compile one integer

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+assert(){
+	expected="$1"
+	input="$2"
+
+	./9cc "$input" > tmp.s
+	cc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" = "$expected" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input => $expected expected, but got $actual"
+		exit 1
+	fi
+}
+
+assert 0 0
+assert 1 1
+assert 42 42
+assert 127 127
+assert 128 128
+assert 129 129
+assert 255 255
+echo OK
+
